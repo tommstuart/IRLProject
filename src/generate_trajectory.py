d37bd234f12bfe52e36ed0446c2b7b70d7a88f56
tommstuart <tommstuart@gmail.com>
2023-04-21 21:17:17 +0100

    Added the ability to analyse the train to testing.ipynb Added the ability to generate multple trajectories.

--- src/generate_trajectory.py
+++ src/generate_trajectory.py
@@ -1,11 +1,12 @@
 current_time = 0.0 
-def generate_trajectory(env, pi, observation_times):
+def generate_trajectory(env, pi, observation_times, n_trajectories = 1):
     actions = []
     observations = [] 
     current_state = env.current_state 
-    for t in range(len(observation_times)):
-        old_state = current_state
-        a = pi(current_state,t)
-        (current_state, _) = env.step(current_state, a, observation_times[t])
-        observations.append([old_state, a, t])
+    for _ in range(n_trajectories):     
+        for t in range(len(observation_times)):
+            old_state = current_state
+            a = pi(current_state,t)
+            (current_state, _) = env.step(current_state, a, observation_times[t])
+            observations.append([old_state, a, t])
     return observations
--- src/policy_walk.py
+++ src/policy_walk.py
@@ -5,8 +5,7 @@ import random
 from policy import choose_a_from_pi
 #ondrej said it's the prior*likelihood. So P(R) * P(O|R) 
 
-def policy_walk(env, observations, step_size = 0.05, n_iters = 10000): #no idea what a normal step size is - they do 0.05 so I guess this is reasonable 
-    n_observations = len(observations) 
+def policy_walk(env, observations, n_observations, step_size = 0.05, n_iters = 10000): #no idea what a normal step size is - they do 0.05 so I guess this is reasonable 
     #Pick a random reward vector - I need to figure out the grid thingy 
     R = np.random.rand(env.n_states, env.n_actions, n_observations) #S x A x T
     #Perform policy iteration 
@@ -23,7 +22,7 @@ def policy_walk(env, observations, step_size = 0.05, n_iters = 10000): #no idea 
         #Maybe do value iteration i.e. combine the two loops of policy iteration and then you don't need to do this check because the policy you compute will be optimal 
         #
         # if is_better(env, n_observations, q_values_tild, pi):
-        ratio = calculate_posterior(env,observations, R_tild, env.R_max, pi_tild)/calculate_posterior(env,observations, R, env.R_max, pi)
+        ratio = calculate_posterior(env,observations,n_observations, R_tild, env.R_max, pi_tild)/calculate_posterior(env,observations, n_observations, R, env.R_max, pi)
         p = min(1,ratio)
         if (random.random() < p):
             R = R_tild 
@@ -52,8 +51,8 @@ def is_better(env, n_observations, q, pi):
 def get_neighbouring_reward(R, step_size): 
     return R + np.random.uniform(-step_size, step_size, R.shape)
 
-def calculate_likelihood(env, observations, R): #look at doing it with log likelihoods 
-    (optimal_pi, optimal_values, optimal_q_values) = learn.policy_iteration(env, len(observations), R) 
+def calculate_likelihood(env, observations, n_observations, R): #look at doing it with log likelihoods 
+    (optimal_pi, optimal_values, optimal_q_values) = learn.policy_iteration(env, n_observations, R) 
     
     boltzmann = Boltzmann(optimal_q_values, env.actions)
     dist = boltzmann.getDistribution(optimal_q_values)
@@ -64,6 +63,6 @@ def calculate_likelihood(env, observations, R): #look at doing it with log likel
 
 #Not sure if my posterior/likelihood calculations are correct
 #P_prior(R) * P(O|R) - not technically the posterior since I don't divide it by the probability of the observation but it doesn't matter. 
-def calculate_posterior(env, observations, R, R_max, pi): 
+def calculate_posterior(env, observations, n_observations, R, R_max, pi): 
     from priors import uniform_prior_probability 
-    return uniform_prior_probability(R, R_max)*calculate_likelihood(env, observations, R)   
+    return uniform_prior_probability(R, R_max)*calculate_likelihood(env, observations, n_observations, R)   
