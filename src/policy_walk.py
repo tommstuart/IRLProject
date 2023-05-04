295b5f12ff81f134fadcb2cdd1849c43d28a5b74
tommstuart <tommstuart@gmail.com>
2023-05-04 13:53:10 +0100

    Full gaussian mcmc step Added the ability to do a full gaussian mcmc step with a covariance matrix. Added a percentage meter to policy walk. Improved the prior to use logs now. Added a sample function which samples from the prior in case you want to use it for initialisation of the reward.
    testing.ipynb has gone through small changes, improved the look of plt graphs added a way to calculate covariance matrices.

--- src/policy_walk.py
+++ src/policy_walk.py
@@ -11,11 +11,14 @@ class PolicyWalk():
         self.observations = observations 
         self.observation_times = observation_times 
         self.alpha = alpha 
+        # self.cov = np.load('cov.npy')
+        # self.rng = np.random.default_rng() 
 
     def get_samples(self, step_size, n_iters):
         n_observations = len(self.observation_times)
         #Initialise reward randomly 
         R = np.random.rand(self.env.n_states, self.env.n_actions, n_observations)
+        # R = self.prior.sample(self.env.n_states, self.env.n_actions, n_observations)
         #Perform policy iteration         
         (pi,values, q_values) = learn.policy_iteration(self.env, n_observations, R)
 
@@ -25,6 +28,7 @@ class PolicyWalk():
         while iter < n_iters: 
             #Perform mcmc step 
             R_tild = gaussian_mcmc_step(R, step_size)
+            # R_tild = self.gaussian_mcmc_step(R)
             
             #There's no harm in passing in pi, values, q_values here because otherwise they'd just end up being random 
             (pi_tild, values_tild, q_values_tild) = learn.policy_iteration(self.env, n_observations, R_tild, pi = pi, values = values, q_values = q_values)
@@ -37,7 +41,7 @@ class PolicyWalk():
                 log_posterior_ratio = 0 
             else: 
                 log_posterior_ratio = math.exp(tild_log_posterior - log_posterior) 
-                p = min(1,log_posterior_ratio)
+            p = min(1,log_posterior_ratio)
 
             acceptance_probs[iter] = p 
 
@@ -50,17 +54,15 @@ class PolicyWalk():
             sampled_rewards[iter] = R
             iter+=1 
 
-            if iter%10000 == 0: 
-                print(iter/n_iters)
+            if math.isclose(iter/n_iters % 0.01, 0, abs_tol = 1e-8):
+                print(iter/n_iters*100, end = "% ")
+
         return sampled_rewards, acceptance_probs
 
     #P_prior(R) * P(O|R) - not technically the posterior since I don't divide it by the probability of the observation but it doesn't matter. 
     def calculate_posterior(self, q_values, R):
-        likelihood, log_likelihood = self.calculate_log_likelihood(q_values)
-        if self.prior(R) == 0: 
-            log_posterior = -np.inf 
-        else: 
-            log_posterior = math.log(self.prior(R)) + log_likelihood 
+        log_likelihood = self.calculate_log_likelihood(q_values)
+        log_posterior = self.prior(R) + log_likelihood
         return log_posterior 
 
     def calculate_log_likelihood(self, q_values): 
@@ -69,6 +71,20 @@ class PolicyWalk():
         for (s,a,t) in self.observations: 
             log_likelihood += boltzmann.logdist[s,a,t]
         return log_likelihood 
+
+    # def gaussian_mcmc_step(self,R):
+    #     # return np.random.multivariate_normal(R.flatten(), self.cov).reshape(R.shape)
+    #     d = np.prod(R.shape) 
+    #     c = 2.4/math.sqrt(d) 
+    #     #Cholesky is faster than SVD but apparently it's less robust?? I think it needs a positive definite matrix, but I'm happy to just 
+    #     #find the nearest positive definite matrix before hand it doesn't make a difference for us and if it improves performance that's great. 
+    #     proposed_R = self.rng.multivariate_normal(R.flatten(), np.multiply(c ** 2, self.cov), method='cholesky').reshape(R.shape)
+
+    #     #I don't think that this is a good idea because then calculating the jumping probabilities is hard and the 
+    #     #probability of jumping to a boundary matrix is way higher and would mean that the acceptance rates wouldn't change much anyway 
+    #     #So I think that it would just add extra time due to the more intensive computation. 
+    #     # clipped_R = np.clip(proposed_R, 0, self.env.R_max)
+    #     return proposed_R
 
 def uniform_mcmc_step(R, step_size): 
     return R + np.random.uniform(-step_size, step_size, R.shape)
--- src/priors.py
+++ src/priors.py
@@ -6,6 +6,8 @@ class UniformPrior():
         self.R_max = R_max 
     def __call__(self, R):
         return uniform_prior_probability(R,self.R_max)
+    def sample(self, shape):
+        return self.R_max*np.random.rand(shape) 
 
 class TimeDependentPrior():
     def __init__(self, observation_times, R_max, sigma): 
@@ -14,9 +16,28 @@ class TimeDependentPrior():
         self.n_observations = len(observation_times)
         self.sigma = sigma 
         
+    #Returns log of the prior 
     def __call__(self, R): 
-        return uniform_prior_probability(R,self.R_max)*self.closeness_requirement(R)
-        
+        #this is fine since uniform_prior returns 0 or 1 
+        if uniform_prior_probability(R,self.R_max) == 0: 
+            return -np.inf
+        else: 
+            return self.closeness_requirement(R)
+    
+    #I don't know if this will introduce too much bias or not.
+    #Randomly sample the first time steps vector and then iterate through, generating the next time steps vector from the previous one
+    #to achieve a vector that should satisfy the prior. 
+    def sample(self, n1, n2, n3):
+        R = self.R_max/2 * np.random.rand(n1,n2,n3) #then I want to coerce it into the correct form 
+        for i in range(self.n_observations-1): 
+            time_diff = abs(self.observation_times[i] - self.observation_times[i+1])
+            cov = (time_diff * self.sigma)*np.identity(R[:,:,0].size)
+            
+            R[:,:,i+1] = np.random.multivariate_normal(R[:,:,i].flatten(), cov).reshape(R[:,:,i].shape)
+            R[:,:,i+1] = np.clip(R[:,:,i+1], 0, self.R_max) #not sure what to do here - it doesn't really matter for this 
+            #but it's not really a true sample
+        return R/2
+
     #Centred at 0 - I've optimised this for using a constant variance rather than a cov matrix 
     def unnormalised_gaussian(self,x,cov_inv): 
         x_flat = x.flatten()
@@ -25,8 +46,12 @@ class TimeDependentPrior():
         return np.exp(-0.5*np.dot(x_flat,x_flat)*cov_inv)
         # return np.exp(-0.5*np.matmul(np.matmul((x_flat-mean).T, cov_inv), x_flat-mean))
 
+    def log_gaussian(self,x,cov_inv): 
+        x_flat = x.flatten() 
+        return -0.5*np.dot(x_flat, x_flat)*cov_inv
+
     def closeness_requirement(self, R): 
-        product = 1 
+        sum = 0 
 
         #Just doing pairwise now for performance sake 
         for i in range(self.n_observations-1): 
@@ -35,12 +60,12 @@ class TimeDependentPrior():
             x = R[:,:,i] - R[:,:,i+1]
             time_diff = abs(self.observation_times[i] - self.observation_times[i+1])
             cov_inv = 1/(time_diff*self.sigma)
-            product *= self.unnormalised_gaussian(x,cov_inv) 
-        return product
+            sum += self.log_gaussian(x,cov_inv)
+        return sum 
             
 
 def uniform_prior_probability(R, R_max):
-    if np.all((0<R) & (R<=R_max)):
+    if np.all((0<=R) & (R<=R_max)):
         return 1 #returning 1 here only works because we're computing a ratio. 
     else: 
         return 0
