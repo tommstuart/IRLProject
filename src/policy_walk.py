6d2bb6389b932c5e9347735f220fdbe0f987258e
tommstuart <tommstuart@gmail.com>
2023-05-09 21:20:37 +0100

    Mainly changes to allow for bulk testing.
    Allows you to test multiple environments, priors and proposal distributions at once.
    
    The changes to policy walk and priors include additional proposal distributions and better ability to change the proposal distribution.

--- src/policy_walk.py
+++ src/policy_walk.py
@@ -5,20 +5,29 @@ import random
 import math
 
 class PolicyWalk(): 
-    def __init__(self, env, prior, observations, observation_times, alpha): 
+    def __init__(self, env, prior, observations, observation_times, alpha, mcmc_step = "gaussian_scalar"): 
         self.env = env 
         self.prior = prior 
         self.observations = observations 
         self.observation_times = observation_times 
         self.alpha = alpha 
-        # self.cov = np.load('cov.npy')
-        # self.rng = np.random.default_rng() 
+        if mcmc_step == "gaussian_scalar": 
+            self.mcmc_step = scalar_gaussian_mcmc_step
+        elif mcmc_step == "gaussian": 
+            self.mcmc_step = self.gaussian_mcmc_step
+        elif mcmc_step == "uniform":
+            self.mcmc_step = uniform_mcmc_step
+        elif mcmc_step == "grid_walk":
+            self.mcmc_step = self.grid_walk_mcmc_step
 
-    def get_samples(self, step_size, n_iters):
+        # self.cov = np.load('cov.npy')
+        self.rng = np.random.default_rng() 
+
+    def get_samples(self, mcmc_cov, n_iters):
         n_observations = len(self.observation_times)
         #Initialise reward randomly 
-        R = np.random.rand(self.env.n_states, self.env.n_actions, n_observations)
-        # R = self.prior.sample(self.env.n_states, self.env.n_actions, n_observations)
+        R = self.prior.sample(self.env.n_states, self.env.n_actions, n_observations)
+        
         #Perform policy iteration         
         (pi,values, q_values) = learn.policy_iteration(self.env, n_observations, R)
 
@@ -27,7 +36,7 @@ class PolicyWalk():
         acceptance_probs = np.zeros(n_iters)
         while iter < n_iters: 
             #Perform mcmc step 
-            R_tild = gaussian_mcmc_step(R, step_size)
+            R_tild = self.mcmc_step(R, mcmc_cov)
             # R_tild = self.gaussian_mcmc_step(R)
             
             #There's no harm in passing in pi, values, q_values here because otherwise they'd just end up being random 
@@ -36,11 +45,12 @@ class PolicyWalk():
             tild_log_posterior = self.calculate_posterior(q_values_tild,R_tild) 
             log_posterior = self.calculate_posterior(q_values, R)
 
-            #This is when the uniform prior part returns 0 - I think this can be omitted if we adapt mcmc step to only propose vectors within [0,R_max]
+            #This might happen for example if using a uniform prior and the mcmc step proposes a reward out of bounds
             if tild_log_posterior == -np.inf: 
                 log_posterior_ratio = 0 
             else: 
                 log_posterior_ratio = math.exp(tild_log_posterior - log_posterior) 
+                
             p = min(1,log_posterior_ratio)
 
             acceptance_probs[iter] = p 
@@ -54,12 +64,12 @@ class PolicyWalk():
             sampled_rewards[iter] = R
             iter+=1 
 
-            if math.isclose(iter/n_iters % 0.01, 0, abs_tol = 1e-8):
-                print(iter/n_iters*100, end = "% ")
+            # if math.isclose(iter/n_iters % 0.01, 0, abs_tol = 1e-8):
+            #     print(iter/n_iters*100, end = "% ")
 
         return sampled_rewards, acceptance_probs
 
-    #P_prior(R) * P(O|R) - not technically the posterior since I don't divide it by the probability of the observation but it doesn't matter. 
+    #P_prior(R) * P(O|R) - un-normalised log posterior 
     def calculate_posterior(self, q_values, R):
         log_likelihood = self.calculate_log_likelihood(q_values)
         log_posterior = self.prior(R) + log_likelihood
@@ -72,24 +82,33 @@ class PolicyWalk():
             log_likelihood += boltzmann.logdist[s,a,t]
         return log_likelihood 
 
-    # def gaussian_mcmc_step(self,R):
-    #     # return np.random.multivariate_normal(R.flatten(), self.cov).reshape(R.shape)
-    #     d = np.prod(R.shape) 
-    #     c = 2.4/math.sqrt(d) 
-    #     #Cholesky is faster than SVD but apparently it's less robust?? I think it needs a positive definite matrix, but I'm happy to just 
-    #     #find the nearest positive definite matrix before hand it doesn't make a difference for us and if it improves performance that's great. 
-    #     proposed_R = self.rng.multivariate_normal(R.flatten(), np.multiply(c ** 2, self.cov), method='cholesky').reshape(R.shape)
+    def gaussian_mcmc_step(self, R, cov):
+        # return np.random.multivariate_normal(R.flatten(), self.cov).reshape(R.shape)
+        d = np.prod(R.shape) 
+        c = 2.4/math.sqrt(d) 
+        #Cholesky is faster than SVD but apparently it's less robust?? I think it needs a positive definite matrix, but I'm happy to just 
+        #find the nearest positive definite matrix before hand it doesn't make a difference for us and if it improves performance that's great. 
+        proposed_R = self.rng.multivariate_normal(R.flatten(), np.multiply(c ** 2, cov), method='cholesky').reshape(R.shape)
 
-    #     #I don't think that this is a good idea because then calculating the jumping probabilities is hard and the 
-    #     #probability of jumping to a boundary matrix is way higher and would mean that the acceptance rates wouldn't change much anyway 
-    #     #So I think that it would just add extra time due to the more intensive computation. 
-    #     # clipped_R = np.clip(proposed_R, 0, self.env.R_max)
-    #     return proposed_R
+        #I don't think that this is a good idea because then calculating the jumping probabilities is hard and the 
+        #probability of jumping to a boundary matrix is way higher and would mean that the acceptance rates wouldn't change much anyway 
+        #So I think that it would just add extra time due to the more intensive computation. 
+        # clipped_R = np.clip(proposed_R, 0, self.env.R_max)
+        return proposed_R
+
+    def grid_walk_mcmc_step(self, R, step_size): 
+        s = np.random.choice(range(self.env.n_states)) 
+        a = np.random.choice(range(self.env.n_actions))
+        t = np.random.choice(range(len(self.observation_times)))
+        step = np.zeros((self.env.n_states, self.env.n_actions, len(self.observation_times)))
+        step[s,a,t] = np.random.choice([-step_size, step_size])
+        return R+step
+
 
 def uniform_mcmc_step(R, step_size): 
     return R + np.random.uniform(-step_size, step_size, R.shape)
 
-#flatten it all - you want smaller perturbations in some places than others
-#We should bound this by the R_max, and suggest things within the time_dependent_prior potentially because otherwise we're just wasting calculations.
-def gaussian_mcmc_step(R, var):
+def scalar_gaussian_mcmc_step(R, var):
     return np.random.normal(R,var) 
+
+
--- src/priors.py
+++ src/priors.py
@@ -1,42 +1,67 @@
 import numpy as np 
-
+from scipy.stats import skewnorm 
 #Essentially just says that our reward lies somewhere between 0 and R_max 
 class UniformPrior(): 
     def __init__(self, R_max): 
         self.R_max = R_max 
+    #Returns log of the prior 
     def __call__(self, R):
-        return uniform_prior_probability(R,self.R_max)
-    def sample(self, shape):
-        return self.R_max*np.random.rand(shape) 
+        if uniform_prior_probability(R,self.R_max) == 0: 
+            return -np.inf 
+        else:
+            return 0
+    def sample(self, n_states, n_actions, traj_length):
+        return self.R_max*np.random.rand(n_states, n_actions, traj_length) 
+
+class SkewSymmetricGaussianPrior(): 
+    def __init__(self, a=5, scale=1.75, loc=0.5):
+        self.a = a 
+        self.scale = scale 
+        self.loc = loc  
+
+    #Really I should do this calculation in log directly to ensure numerical stability but 
+    #I'm logging each call immediately so it'd have to suggest a reward miles away from the 
+    #normal range to get a pdf of literally 0 here which should never happen for normal sigma values
+    #and normal priors etc. So as long as sigma is kept low, which I will be doing, this shouldn't be an issue 
+    def __call__(self,R):         
+        return np.sum(np.log(skewnorm.pdf(R, a = self.a, scale = self.scale, loc = self.loc)))
+    
+    def sample(self, n_states, n_actions, traj_length): 
+        return skewnorm.rvs(a = self.a, scale = self.scale, loc = self.loc, size = n_states * n_actions * traj_length).reshape((n_states, n_actions, traj_length))
 
 class TimeDependentPrior():
-    def __init__(self, observation_times, R_max, sigma): 
+    def __init__(self, observation_times, R_max, sigma, full = False): 
         self.R_max = R_max 
         self.observation_times = observation_times 
         self.n_observations = len(observation_times)
         self.sigma = sigma 
+        self.full = full 
+        self.ssgp = SkewSymmetricGaussianPrior()
         
     #Returns log of the prior 
     def __call__(self, R): 
         #this is fine since uniform_prior returns 0 or 1 
-        if uniform_prior_probability(R,self.R_max) == 0: 
-            return -np.inf
-        else: 
-            return self.closeness_requirement(R)
+        # if uniform_prior_probability(R,self.R_max) == 0: 
+        #     return -np.inf
+        # else: 
+        #     return self.closeness_requirement(R)
+        return self.ssgp(R) + self.closeness_requirement(R)
+        # return self.closeness_requirement(R)  
     
-    #I don't know if this will introduce too much bias or not.
-    #Randomly sample the first time steps vector and then iterate through, generating the next time steps vector from the previous one
-    #to achieve a vector that should satisfy the prior. 
+    # I don't know if this will introduce too much bias or not.
+    # Randomly sample the first time steps vector and then iterate through, generating the next time steps vector from the previous one
+    # to achieve a vector that should satisfy the prior. 
     def sample(self, n1, n2, n3):
-        R = self.R_max/2 * np.random.rand(n1,n2,n3) #then I want to coerce it into the correct form 
+        # R = self.R_max*np.random.rand(n1,n2,n3) #then I want to coerce it into the correct form 
+        R = self.ssgp.sample(n1,n2,n3)
         for i in range(self.n_observations-1): 
             time_diff = abs(self.observation_times[i] - self.observation_times[i+1])
-            cov = (time_diff * self.sigma)*np.identity(R[:,:,0].size)
+            cov = ((time_diff * self.sigma)**2)*np.identity(R[:,:,0].size)
             
             R[:,:,i+1] = np.random.multivariate_normal(R[:,:,i].flatten(), cov).reshape(R[:,:,i].shape)
-            R[:,:,i+1] = np.clip(R[:,:,i+1], 0, self.R_max) #not sure what to do here - it doesn't really matter for this 
+            # R[:,:,i+1] = np.clip(R[:,:,i+1], 0, self.R_max) #not sure what to do here - it doesn't really matter for this 
             #but it's not really a true sample
-        return R/2
+        return R
 
     #Centred at 0 - I've optimised this for using a constant variance rather than a cov matrix 
     def unnormalised_gaussian(self,x,cov_inv): 
@@ -53,15 +78,23 @@ class TimeDependentPrior():
     def closeness_requirement(self, R): 
         sum = 0 
 
-        #Just doing pairwise now for performance sake 
-        for i in range(self.n_observations-1): 
-            # for j in range(n_observations): 
-            #     if i != j: 
-            x = R[:,:,i] - R[:,:,i+1]
-            time_diff = abs(self.observation_times[i] - self.observation_times[i+1])
-            cov_inv = 1/(time_diff*self.sigma)
-            sum += self.log_gaussian(x,cov_inv)
-        return sum 
+        if self.full: 
+            for i in range(self.n_observations-1): 
+                for j in range(self.n_observations-1): 
+                    if i != j: 
+                        x = R[:,:,i] - R[:,:,i+1]
+                        time_diff = abs(self.observation_times[i] - self.observation_times[i+1])
+                        cov_inv = 1/((time_diff*self.sigma)**2)
+                        sum += self.log_gaussian(x,cov_inv)
+            return sum 
+        else: 
+            #Just do pairwise
+            for i in range(self.n_observations-1): 
+                x = R[:,:,i] - R[:,:,i+1]
+                time_diff = abs(self.observation_times[i] - self.observation_times[i+1])
+                cov_inv = 1/((time_diff*self.sigma)**2)
+                sum += self.log_gaussian(x,cov_inv)
+            return sum 
             
 
 def uniform_prior_probability(R, R_max):
